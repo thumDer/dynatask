5deab6cbd0240e1131c6f5fff8683ceeee5019ab
tamder <ranger41@gmail.com>
2020-02-23 14:54:59 +0100

    project name change

---
+++ dynatask/__main__.py
@@ -0,0 +1,31 @@
+import dynatask
+from defaultconfig import configPath
+import logging
+from os import path
+
+
+if __name__ == '__main__':
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler('dyntocal.log', 'w', 'utf-8')
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s %(message)s'))
+    root_logger.addHandler(handler)
+
+    if not path.exists(configPath):
+        configError = (
+            'Config file not found, please run'
+            ' ./dynatask/defaultconfig.py to'
+            ' deploy it, and fill the default settings manually'
+            ' as per the documentation.'.format(configPath))
+        print(configError)
+        logging.error(configError)
+        exit()
+
+    logging.info('Started')
+    print("Started")
+
+    # dynatask.run()
+
+    logging.info('Finished')
+    print("Finished")
---
+++ dynatask/defaultconfig.py
@@ -0,0 +1,36 @@
+import configparser
+from os import path
+
+config = configparser.ConfigParser()
+configPath = './dynatask/config.ini'
+
+config['dynalist'] = {'file_url': 'https://dynalist.io/api/v1/file/list',
+                      'doc_url': 'https://dynalist.io/api/v1/doc/read',
+                      'api_key': '',
+                      'tasklist_tag': '#tlist',
+                      'task_tag': '#task',
+                      'alarm_prefix': '#alarm'}
+
+config['google'] = {'task_url': '',
+                    'calendar_url': '',}
+
+config['caldav'] = {'task_url': '',
+                    'calendar_url': '',
+                    'user': '',
+                    'password': ''}
+
+
+def deploy():
+    with open(configPath, 'w') as configfile:
+        config.write(configfile)
+
+
+if __name__ == '__main__':
+    if not path.exists(configPath):
+        deploy()
+        print('Config file created at {}'.format(configPath))
+    else:
+        print(
+            'Found existing config file at {}, '
+            'please remove it before deploying a new one.'
+            .format(configPath))
